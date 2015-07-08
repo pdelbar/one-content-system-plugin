bce3ecf06eed7ab18e64da25b8a67d9d953174ba
Paul Delbar <paul.delbar@gmail.com>
2015-07-08 22:13:28 +0200

    refactored One_Type to One_Scheme_Attribute_Type

--- core/type/calculated/interface_tbd.php
+++
@@ -1,7 +0,0 @@
-<?php
-Interface One_Type_Calculated_Interface
-{
-	public function calculate(One_Model $model);
-	public static function orderList(array $list = array(), $direction = 'asc');
-	public function getNativeComposition();
-}
